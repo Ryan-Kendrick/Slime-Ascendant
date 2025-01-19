890d9d992e25d069c46407b3111969d3abd4dbbe
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-01-19 21:58:13 +1300

    add matching animation for overlay

--- components/metapanel/upgrades/levelUpButton.tsx
+++ components/metapanel/upgrades/levelUpButton.tsx
@@ -13,15 +13,12 @@ export default function LevelUpButton({ id, onClick, currentLevel, levelUpCost, 
   return (
     <div className="border-2 border-amber-900 ring-1 ring-amber-950">
       <div className="relative border-4 border-amber-950 bg-amber-950">
-        <div className="absolute ml-1 mt-[0.13rem] rounded-tl-sm bg-white/50 inset-x-0 top-0 bottom-3/4 z-20 pointer-events-none" />
-        <div className="absolute ml-1 rounded-bl bg-gradient-to-t from-white/0 to-white/50 inset-x-0 top-1/4 bottom-1/2 z-20 pointer-events-none" />
-        <div className="absolute z-20 pointer-events-none" />
         <button
           disabled={!isAffordable}
           id={id}
           className={clsx(
             // Base
-            "flex flex-col items-center py-2 px-4 min-w-32 text-white cursor-hand",
+            "flex flex-col items-center py-2 px-4 min-w-32 text-white text-lg cursor-hand",
             "border-2 border-amber-300",
             "transition-all duration-75",
             "shadow-[0_0_8px_0px_rgba(251,191,36,0.9),inset_0_0_4px_-1px_rgba(251,191,36,0.8)]",
@@ -31,6 +28,7 @@ export default function LevelUpButton({ id, onClick, currentLevel, levelUpCost, 
             "enabled:hover:shadow-[0_0_6px_0px_rgba(251,191,36,1),inset_0_0_6px_-1px_rgba(251,191,36,0.9)]",
 
             // Pressed
+            "peer",
             "enabled:active:translate-y-0.5",
             "enabled:active:shadow-[0_0_3px_0px_rgba(251,191,36,0.8),inset_0_0_8px_-1px_rgba(251,191,36,1)]",
             "enabled:active:border-amber-400",
@@ -43,6 +41,8 @@ export default function LevelUpButton({ id, onClick, currentLevel, levelUpCost, 
             <img className="w-[1.4rem] inline-block self-center" src={`${coinURL}`} alt="gold coin" /> {levelUpCost}
           </span>
         </button>
+        <div className="absolute ml-1 mt-1 mr-1 rounded-t-sm bg-white/50 inset-x-0 top-0 bottom-3/4 z-20 transition-transform duration-75 peer-enabled:peer-active:translate-y-0.5  pointer-events-none" />
+        <div className="absolute ml-1 mr-1 rounded-bl bg-gradient-to-t from-white/0 to-white/50 inset-x-0 top-1/4 bottom-1/2 z-20 transition-transform duration-75 peer-enabled:peer-active:translate-y-0.5 pointer-events-none" />
       </div>{" "}
     </div>
   )
