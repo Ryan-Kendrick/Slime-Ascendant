d5f81835460f6a4b9655f949c42b066a52ffb3e9
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-02-15 22:16:59 +1300

    fix incorrect loading screen z-index

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
   if (!loading) return null
 
   return (
-    <div className="fixed w-full h-full z-90 flex items-center justify-center bg-black/30 backdrop-blur-sm">
+    <div className="fixed w-full h-full z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
       <div className="text-6xl pl-[3ch] text-white">LOADING...</div>
     </div>
   )
