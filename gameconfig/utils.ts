4f08bcd5297b140e5d25023fb11472e15618889c
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-06-21 18:53:21 +1200

    save migration debug

--- gameconfig/utils.ts
+++ gameconfig/utils.ts
@@ -107,7 +107,7 @@ Your save from ${saveVersion} doesn't quite fit into the ${currentVersion} world
 The time has come to start a brand new adventure.`)
       }, 100)
       return undefined
-    } else if (Number(saveMinorVersion) === 4) {
+    } else if (Number(saveMinorVersion) === 5) {
       setTimeout(() => {
         alert(`
 Attention Slime Slayer!
