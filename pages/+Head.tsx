bef97e9d5311e6a9bff4173319ec643536758552
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-07-20 14:21:05 +1200

    move fonts out of vite again to enable preloading

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -5,8 +5,8 @@ import paytoneOneURL from "../assets/fonts/paytone-one.woff2"
 export default function HeadDefault() {
   return (
     <>
-      <link rel="preload" href={sigmarURL} as="font" type="font/woff2" crossOrigin="anonymous" />
-      <link rel="preload" href={paytoneOneURL} as="font" type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/Sigmar.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/paytone-one.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
     </>
   )
 }
