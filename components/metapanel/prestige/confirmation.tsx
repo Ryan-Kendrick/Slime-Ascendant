4bf56f1edfc1848b6e09b206d3bbfac267d30814
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-08-10 15:14:05 +1200

    add prestige confirmation

--- components/metapanel/prestige/confirmation.tsx
+++ components/metapanel/prestige/confirmation.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from "../../../redux/hooks"
 import { selectGold, selectPendingPPurchases, selectPlasma } from "../../../redux/playerSlice"
 import { selectAchievementModifier, selectHeroState } from "../../../redux/shared/heroSelectors"
 import { selectCurrentZoneNumber } from "../../../redux/zoneSlice"
+import clsx from "clsx/lite"
 
 interface Props {
   initiatePrestige: () => void
@@ -23,53 +24,70 @@ export default function Confirmation({ initiatePrestige }: Props) {
   const unspentPlasma = useAppSelector(selectPlasma)
   const achievementMod = Math.round(useAppSelector(selectAchievementModifier) * 100)
   const pendingUpgrades = useAppSelector(selectPendingPPurchases)
+  const actuallyPendingUpgrades = Object.entries(pendingUpgrades).length > 0
 
   return (
     <div className="flex h-full flex-col">
-      <h2 className="mb-4 self-center text-2xl font-bold">Go backwards to go forwards</h2>
+      <h2 className="mb-4 self-center border-b-2 border-frost text-2xl font-bold text-frost">
+        Go backward to go forward
+      </h2>
       <div className="flex justify-around">
         <div className="flex w-[26.4vw] flex-col">
-          <ul className="loss text-red-600">
-            <h3 className="line-y-bottomright line-y-bottomleft mb-1 text-3xl font-bold">You will lose</h3>
-            <li className="line-x-bottom line-x-top flex justify-between text-lg">
+          <ul className="loss">
+            <h3 className="line-y-bottomright line-y-bottomleft text-shadow-long mb-1 text-center text-3xl font-bold text-red-500">
+              You will lose
+            </h3>
+            <li className="line-x-bottom line-x-top flex justify-between p-1 text-lg text-frost">
               <h4>Gold</h4>
-              <p>{gold}</p>
+              <p className="font-bold text-red-500">{gold}</p>
             </li>
-            <li className="line-x-bottom flex justify-between text-lg">
+            <li className="line-x-bottom flex justify-between p-1 text-lg text-frost">
               <h4>Upgrades</h4>
-              <p>{upgrades}</p>
+              <p className="font-bold text-red-500">{upgrades}</p>
             </li>
-            <li className="line-x-bottom flex justify-between text-lg">
+            <li className="line-x-bottom flex justify-between p-1 text-lg text-frost">
               <h4>Zone progress</h4>
-              <p>{zone}</p>
+              <p className="font-bold text-red-500">{zone}</p>
             </li>
           </ul>
         </div>
         <div className="flex w-[26.4vw] flex-col">
-          <ul className="keep text-amber-700">
-            <h3 className="line-y-bottomright mb-1 text-3xl font-bold">You will keep</h3>
-            <li className="line-x-bottom line-x-top flex justify-between text-lg">
+          <ul className="keep">
+            <h3 className="line-y-bottomright text-shadow-long mb-1 text-center text-3xl font-bold text-amber-500">
+              You will keep
+            </h3>
+            <li className="line-x-bottom line-x-top flex justify-between p-1 text-lg text-frost">
               <h4>Unspent plasma</h4>
-              <p>{unspentPlasma}</p>
+              <p className="font-bold text-amber-500">{unspentPlasma}</p>
             </li>
-            <li className="line-x-bottom flex justify-between text-lg">
+            <li className="line-x-bottom flex justify-between p-1 text-lg text-frost">
               <h4>Achievement Damage</h4>
-              <p>+{achievementMod}%</p>
+              <p className="font-bold text-amber-500">+{achievementMod}%</p>
             </li>
           </ul>
         </div>
         <div className="flex w-[26.4vw] flex-col">
-          <ul className="gain text-islam">
-            <h3 className="line-x-bottom-fromright mb-1 text-3xl font-bold">You will gain</h3>
-            <li className="line-x-top line-y-right text-lg">
-              <h4 className="line-x-bottom-fromright text-center">Prestige upgrades</h4>
-              {Object.entries(pendingUpgrades).length > 0 &&
+          <ul className="gain text-green-500">
+            <h3 className="line-x-bottom-fromright text-shadow-long mb-1 text-center text-3xl font-bold">
+              You will gain
+            </h3>
+            <li className="line-x-top line-y-right text-xl">
+              <h4
+                className={clsx(
+                  "line-x-bottom-fromright p-1 text-center font-extrabold tracking-wider",
+                  actuallyPendingUpgrades
+                    ? "font-outline-electricBlue text-shadow-dark"
+                    : "font-outline-none text-shadow-none text-green-100/70",
+                )}>
+                Prestige upgrades
+              </h4>
+              {actuallyPendingUpgrades &&
                 Object.entries(pendingUpgrades).map(([upgradeName, upgrade]) => (
                   <li key={upgradeName} className="line-x-bottom mx-4 mt-2 flex justify-between text-lg">
-                    <span className="font-outline-electricBlue font-sans text-2xl font-extrabold tracking-wide text-white">
+                    <span className="font-outline-electricBlue mb-1 font-sans text-2xl font-extrabold tracking-wide text-white">
                       {UPGRADE_CONFIG.prestigeUpgrades[upgradeName as PrestigeUpgradeId].displayName}
                     </span>
-                    <span className="text-frost">x{upgrade.purchaseCount}</span>
+                    <span className="self-end text-xl text-frost">x{upgrade.purchaseCount}</span>
                   </li>
                 ))}
             </li>
