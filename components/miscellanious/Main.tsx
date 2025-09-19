404234a7155aed8612a806a6f6c90be113ad7465
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-09-19 18:33:17 +1200

    fix debug catchup delta value

--- components/miscellanious/Main.tsx
+++ components/miscellanious/Main.tsx
@@ -7,7 +7,7 @@ import Navigation from "../nav/navigation"
 import { GameEngineProvider } from "./Engine"
 
 export default function Main() {
-  const delta = true //useAppSelector(selectLongCatchupDelta)
+  const delta = useAppSelector(selectLongCatchupDelta)
 
   return (
     <>
