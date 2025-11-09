778f77779553e22434f2353292f99a6156031c9c
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-11-10 08:24:52 +1300

    fix import path

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,14 +1,14 @@
 import "./index/style.css"
 import "./index/tailwind.css"
 import React from "react"
-import { store } from "../redux/store"
+import { store } from "../../redux/store"
 import { Provider } from "react-redux"
-import Navigation from "../components/nav/navigation"
+import Navigation from "../../components/nav/navigation"
 import ReactModal from "react-modal"
-import Loading from "../components/miscellanious/Loading"
-import Wrapper from "../components/miscellanious/Wrapper"
-import Fading from "../components/miscellanious/Fading"
-import Main from "../components/miscellanious/Main"
+import Loading from "../../components/miscellanious/Loading"
+import Wrapper from "../../components/miscellanious/Wrapper"
+import Fading from "../../components/miscellanious/Fading"
+import Main from "../../components/miscellanious/Main"
 
 export default function Page() {
   ReactModal.setAppElement("#root")
